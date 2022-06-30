d988945cb0a5ec763963ff97fae1d11134e97665
tarasbandura <taras190789@gmail.com>
2022-06-30 19:05:35 +0300

    doc

--- src/main/java/com/example/javadoc0032022/controllers/DocumentController.java
+++ src/main/java/com/example/javadoc0032022/controllers/DocumentController.java
@@ -1,9 +1,13 @@
 package com.example.javadoc0032022.controllers;
 
 import com.example.javadoc0032022.models.Document;
+import com.example.javadoc0032022.models.Role;
 import com.example.javadoc0032022.models.User;
 import com.example.javadoc0032022.models.enums.DocumentStatus;
+import com.example.javadoc0032022.models.enums.ERole;
+import com.example.javadoc0032022.payload.response.DocUsersSubscribeResponse;
 import com.example.javadoc0032022.payload.response.MessageResponse;
+import com.example.javadoc0032022.repository.RoleRepository;
 import com.example.javadoc0032022.services.DocumentService;
 import com.example.javadoc0032022.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,21 +27,84 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 @RestController
 @RequestMapping("/api/doc")
 @AllArgsConstructor
 public class DocumentController {
 
-    private static final String NEW_DOC_FILE = "src/main/resources/static/documents/docECP.docx";
+    //    private static final String NEW_DOC_FILE = "src/main/resources/static/documents/docECP.docx";
+    private static final String NEW_DOC_FILE = "/dev/home/documents/docECP.docx";
     private DocumentService documentService;
     private UserService userService;
+    private RoleRepository roleRepository;
+
+    @Operation(summary = "Получение всех документов", description = "Документ передается в байтах")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Document.class)))
+    @GetMapping
+    public ResponseEntity<List<Document>> findAllDocuments() {
+        return ResponseEntity.ok(documentService.findAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> findDocument(@PathVariable int id) {
+        Optional<Document> document = documentService.findById(id);
+        if (document.isEmpty())
+            return new ResponseEntity<>(new MessageResponse("Document not found"), HttpStatus.NOT_FOUND);
+
+        return ResponseEntity.ok(document.get());
+    }
+
+    @GetMapping("/download_all_documents")
+    public ResponseEntity<?> downloadZip() throws IOException {
+        Optional<User> user = userService.findByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
+//        Optional<User> user = userService.findById(2);
+        if (user.isEmpty())
+            return new ResponseEntity<>(new MessageResponse("User not found"), HttpStatus.NOT_FOUND);
+        Role roleUser = roleRepository.findByRole(ERole.ROLE_USER).get();
+        Role roleEmployee = roleRepository.findByRole(ERole.ROLE_EMPLOYEE).get();
+
+        ByteArrayOutputStream bo = new ByteArrayOutputStream();
+        ZipOutputStream zipOut = new ZipOutputStream(bo);
+
+        if (user.get().getRoles().contains(roleUser)) {
+            for (Document item : user.get().getDocumentReceiverUser()) {
+                ZipEntry zipEntry = new ZipEntry(item.getDocName());
+                zipOut.putNextEntry(zipEntry);
+                zipOut.write(item.getFile());
+                zipOut.closeEntry();
+            }
+        } else if (user.get().getRoles().contains(roleEmployee)) {
+            for (Document item : user.get().getDocumentSenderList()) {
+                ZipEntry zipEntry = new ZipEntry(item.getDocName());
+                zipOut.putNextEntry(zipEntry);
+                zipOut.write(item.getFile());
+                zipOut.closeEntry();
+            }
+        }
+
+        zipOut.close();
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=documents.zip")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM).contentLength(bo.size()).body(bo.toByteArray());
+    }
+
+    @GetMapping("/view_users_subscribe/{documentId}")
+    public ResponseEntity<?> viewUsersSubscribe(@PathVariable int documentId) {
+        Optional<Document> document = documentService.findById(documentId);
+        if (document.isEmpty())
+            return new ResponseEntity<>(new MessageResponse("Document not found"), HttpStatus.NOT_FOUND);
+
+        if (document.get().getStatus().equals(DocumentStatus.SIGNED)) {
+            return ResponseEntity.ok(new DocUsersSubscribeResponse(document.get().getId(), document.get().getStatus(),
+                    document.get().getSenderUser(), document.get().getReceiverUser()));
+        } else return ResponseEntity.ok(new MessageResponse("the document is not signed"));
+    }
 
     @Operation(summary = "метод создания документа")
     @ApiResponse(responseCode = "200", description = "Загрузка файла",
@@ -50,22 +117,21 @@ public class DocumentController {
                 .contentType(MediaType.APPLICATION_OCTET_STREAM).contentLength(file.length()).body(resource);
     }
 
+    @Operation(summary = "Скачать документ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Документ не найден",
+                    content = @Content(schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "200", description = "Документ успешно загружен")
+    })
     @GetMapping("/download/{documentId}")
-    public ResponseEntity<?> downloadDoc(@PathVariable int documentId) {
+    public ResponseEntity<?> downloadDoc(@Parameter(required = true, description = "Document ID") @PathVariable int documentId) {
         Optional<Document> document = documentService.findById(documentId);
         if (document.isEmpty())
             return new ResponseEntity<>(new MessageResponse("Document not found"), HttpStatus.NOT_FOUND);
 
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" +
-                document.get().getDocName()).contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        document.get().getDocName()).contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .contentLength(document.get().getFile().length).body(document.get().getFile());
-    }
-
-    @Operation(summary = "Получение всех документов", description = "Документ передается в байтах")
-    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Document.class)))
-    @GetMapping
-    public ResponseEntity<List<Document>> findAllDocuments() {
-        return ResponseEntity.ok(documentService.findAll());
     }
 
     @Operation(summary = "Проверка подписи документа")
@@ -124,8 +190,17 @@ public class DocumentController {
 
     }
 
+    @Operation(summary = "Отправка документа на соглосование с клиентом")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Документ или юзер не найден",
+                    content = @Content(schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "200", description = "Документ отправлен на соглосование")
+    })
     @PutMapping("/send_to_client_agreed/{userId}/{documentId}")
-    public ResponseEntity<MessageResponse> sendToClientAgreed(@PathVariable int userId, @PathVariable int documentId) {
+    public ResponseEntity<MessageResponse> sendToClientAgreed(@Parameter(required = true, description = "User ID")
+                                                              @PathVariable int userId,
+                                                              @Parameter(required = true, description = "Document ID")
+                                                              @PathVariable int documentId) {
         Optional<User> user = userService.findById(userId);
         Optional<Document> document = documentService.findById(documentId);
         if (user.isEmpty() || document.isEmpty())
@@ -137,8 +212,17 @@ public class DocumentController {
         return ResponseEntity.ok(new MessageResponse("sent to client " + userId));
     }
 
+    @Operation(summary = "Отправка документа на подпись")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Документ или юзер не найден",
+                    content = @Content(schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "200", description = "Документ отправлен на подпись")
+    })
     @PutMapping("/send_to_client_subscribe/{userId}/{documentId}")
-    public ResponseEntity<?> sendToClientSubscribe(@PathVariable int userId, @PathVariable int documentId) {
+    public ResponseEntity<?> sendToClientSubscribe(@Parameter(required = true, description = "User ID")
+                                                   @PathVariable int userId,
+                                                   @Parameter(required = true, description = "Document ID")
+                                                   @PathVariable int documentId) {
         Optional<User> user = userService.findById(userId);
         Optional<Document> document = documentService.findById(documentId);
         if (user.isEmpty() || document.isEmpty())
@@ -150,8 +234,18 @@ public class DocumentController {
         return ResponseEntity.ok(new MessageResponse("sent to client " + userId));
     }
 
+    @Operation(summary = "Смена статуса документа",
+            description = "статус документа меняется через индекс: 0 - документ согласован, 1 - документ подписан, " +
+                    "2 - отказано, 3 - отменено")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Документ или индекс не найден",
+                    content = @Content(schema = @Schema(implementation = MessageResponse.class))),
+            @ApiResponse(responseCode = "200", description = "Статус изменен",
+                    content = @Content(schema = @Schema(implementation = Document.class)))
+    })
     @PutMapping("/change_status/{idDocument}/{index}")
-    public ResponseEntity<?> changeStatusDocument(@PathVariable int index, @PathVariable int idDocument) {
+    public ResponseEntity<?> changeStatusDocument(@Parameter(required = true, description = "Index") @PathVariable int index,
+                                                  @Parameter(required = true, description = "Document ID") @PathVariable int idDocument) {
         Optional<Document> document = documentService.findById(idDocument);
         if (document.isEmpty())
             return new ResponseEntity<>(new MessageResponse("Document not found"), HttpStatus.NOT_FOUND);
@@ -176,8 +270,10 @@ public class DocumentController {
         return ResponseEntity.ok(document.get());
     }
 
+    @Operation(summary = "Удаление документа")
     @DeleteMapping("{id}")
-    public ResponseEntity<MessageResponse> deleteDocument(@PathVariable int id) {
+    public ResponseEntity<MessageResponse> deleteDocument(@Parameter(required = true, description = "Document ID")
+                                                          @PathVariable int id) {
         documentService.deleteById(id);
         return ResponseEntity.ok(new MessageResponse("Document deleted"));
     }
